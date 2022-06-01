b8ab97598c3712bda4d8dbb9c02bed614721b8df
jayaprasad356 <jayaprasad356@gmail.com>
2022-06-01 09:23:53 +0530

    sql added

--- care.sql
+++ care.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2022 at 11:47 PM
+-- Generation Time: Jun 01, 2022 at 05:52 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
